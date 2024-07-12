123956f1f91c12500ebfcf33cc3354a6ffa029dd
LuaraNabor <luaranaboraci@gmail.com>
2024-07-12 09:50:40 -0300

    Add files via upload

---
+++ MyDossie.py
@@ -0,0 +1,17 @@
+#Declaração de variáveis...
+MyName = "Luara"
+MyHeight = 1.60
+MyAge = 30
+MyEmail = "luaramarques@gmail.com"
+MyPhone = "212345-0045"
+MyContHome = 1
+MyAccount = 1234.6
+
+#Exibição do conteúdo das variáveis...
+print(f"Meu nome é{MyName}.") 
+print(f"Minha altura é de {MyHeight} metros.")
+print(f"minha idade é {MyAge} anos")
+print(f"Meu email é  {MyEmail}.") 
+print(f"O Saldo da minha conta é:{MyAccount}.") 
+print(f"A quantidade de casa é: {MyContHome}.") 
+print(f"O meu telefone é {MyPhone}.") 
---
+++ MyDossie1.py
@@ -0,0 +1,22 @@
+#inserção de dados...
+print("Digite o seu nome:")
+MyName = str(input())
+print("Digite a sua idade:")
+MyAge = int(input())
+print("Digite a sua altura")
+MyHeight = float(input())
+
+#Declaração de variáveis...
+MyEmail = "luaramarques@gmail.com"
+MyPhone = "212345-0045"
+MyContHome = 1
+MyAccount = 1234.6
+
+#Exibição do conteúdo das variáveis...
+print(f"Meu nome é   {MyName}.") 
+print(f"Minha altura é de {MyHeight} metros.")
+print(f"minha idade é     {MyAge} anos")
+print(f"Meu email é  {MyEmail}.") 
+print(f"O Saldo da minha conta é: {MyAccount}.") 
+print(f"A quantidade de casa é:   {MyContHome}.") 
+print(f"O meu telefone é:         {MyPhone}.") 
